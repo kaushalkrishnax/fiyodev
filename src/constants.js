71caf332feb8d7479c2444adcb67273a6fd7a925
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-03-25 18:01:27 +0530

    Updated Saavn API URI temporarily

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const FIYOGQL_BASE_URI = "https://fiyogql.onrender.com/graphql"
-export const FIYOSAAVN_BASE_URI = "https://fiyosaavn.vercel.app/api";
+// export const FIYOSAAVN_BASE_URI = "https://fiyosaavn.vercel.app/api";
+export const FIYOSAAVN_BASE_URI = "https://saavn.dev/api";
