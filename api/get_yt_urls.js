7ffc80d3626f1a5b86a8e3741281fa2ab95fd922
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-03-28 16:21:46 +0530

    updated responseing

--- api/get_yt_urls.js
+++ api/get_yt_urls.js
@@ -2,6 +2,15 @@ import { load } from "cheerio";
 
 export default async function handler(req, res) {
   try {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+      return res.status(200).end();
+    }
+
     if (req.method !== "GET")
       return res.status(405).json({ error: "Method Not Allowed" });
     const { videoId } = req.query;
